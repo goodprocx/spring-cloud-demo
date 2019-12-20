986bd5bc7db9146d15e6e0ceb085c56406cad125
procx <372877498@qq.com>
2019-12-20 15:57:09 +0800

    111111111111

--- feign/src/main/java/com/procx/cloud/feign/rpc/FeignInterface.java
+++ feign/src/main/java/com/procx/cloud/feign/rpc/FeignInterface.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "ribbon-provider")
+@FeignClient(value = "ribbon-provider",path = "/ribbon-provider")
 public interface FeignInterface {
 
     @RequestMapping(value = "hello",method = RequestMethod.GET)
